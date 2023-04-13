9dd7f3f2464c1bbe7abe7ea583d7f254b8c38c85
grisdi160999 <moralesgreydi16@gmail.com>
2023-04-12 19:36:28 -0600

    Solucion del ejercicio 18

---
+++ ejercicio_18.js
@@ -0,0 +1,8 @@
+
+function convertirAMinusculas(cadena) {
+  return cadena.toLowerCase();
+}
+
+// Ejemplo de uso
+let texto = "Hola Mundo";
+let resultado = convertirAMinusculas(texto); // resultado será "hola mundo"
